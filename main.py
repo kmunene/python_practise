aadb6547818ff30181cb88ce0d66626e282dcb36
kelvin munene <kmunene529@gmail.com>
2024-10-31 16:07:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ main_page_url = "https://jumia.co.ke/"
 
 main_page_data = requests.get(main_page_url)
 
+if main_page_data.status_code != 200:
+    print("no bots mofo")
+
 main_page_soup = BeautifulSoup(main_page_data.text, 'html.parser')
 
 mini_site_urls = [a['href'] for a in main_page_soup.find_all('a', href=True)]
